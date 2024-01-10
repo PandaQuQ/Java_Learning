9e20085edeff686336917c6273a1403c0edd9bde
Panda <mabinsoft@vip.qq.com>
2024-01-10 10:14:38 +0000

    Update some practices

--- Term1/src/Tutorial010424.java
+++ Term1/src/Tutorial010424.java
@@ -1,9 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Tutorial010424 {
     public static void main(String[] args) {
-        Q1();
+        //Q1();
+        //Q2();
+        //Q3();
+        //Q4();
+        //Q5();
+        //Q5_multiplication();
+        //Q5_division();
+        
     }
 
     public static void Q1(){
@@ -38,8 +47,140 @@ public class Tutorial010424 {
         }
     }
 
+    public static void Q2() {
+        // Read integer values from an array and display it in reverse order.
+        int[] intArray = new Random().ints(5,0,99).toArray();
+        for (int i : intArray){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        int[] newIntArray = new int[intArray.length];
+        for (int i = 0; i < intArray.length; i++){
+            newIntArray[i] = intArray[intArray.length - 1 - i];
+        }
+
+        for (int i : newIntArray){
+            System.out.print(i + " ");
+        }
+
+        
 
 
 
+    }
+    
+    public static void Q3() {
+        String[] strArray = {"bcd","abd","jude","bcd","oiu"};
+        ArrayList dupList = new ArrayList<String>();
+        for (int i = 0; i < strArray.length-1; i++){
+
+            for(int j = i + 1; j < strArray.length;j++){
+                if (strArray[i] == strArray[j]) {
+                    dupList.add(strArray[i]);
+                }
+            }
+        }
+        System.out.println(dupList);
+    }
+
+    public static void Q4() {
+        String[] strArray1 = {"Python","JAVA","PHP"};
+        String[] strArray2 = {"MYSQL","Oracle","PHP"};
+        ArrayList commonElement = new ArrayList<String>();
+
+        for(int i = 0; i < strArray1.length;i++){
+            for(int j = 0; j < strArray2.length;j++){
+                if (strArray1[i]==strArray2[j]) {
+                    commonElement.add(strArray1[i]);
+                }
+            }
+        }
+        
+        System.out.println(commonElement);
+
+    }
+
+    public static int[][] matrixGen(int size_row,int size_col){
+        int[][] matrix = new int[size_row][size_col];
+        for(int i =0;i<size_row;i++){
+            matrix[i] = new Random().ints(size_col,0,9).toArray();
+        }
+        return matrix;
+    }
+
+    public static void matrixDisplay(int[][] matrix){
+        for (int i=0;i<matrix.length;i++){
+            for(int j = 0; j<matrix[i].length;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+            
+        }
+        System.out.println();
+    }
+
+    public static void Q5(){
+        
+        int [][] matrix1 = matrixGen(3,3);
+        matrixDisplay(matrix1);
+        int [][] matrix2 = matrixGen(3, 3);
+        matrixDisplay(matrix2);
+        int [][] matrix_sum = new int[3][3];
+        for(int i = 0; i<3;i++){
+            for(int j = 0; j<3;j++){
+                matrix_sum[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        matrixDisplay(matrix_sum);
+        
+    }
+    
+    public static void Q5_multiplication(){
+        
+        int [][] matrix1 = matrixGen(3,3);
+        matrixDisplay(matrix1);
+        int [][] matrix2 = matrixGen(3, 3);
+        matrixDisplay(matrix2);
+        int [][] matrix_sum = new int[3][3];
+        for(int i = 0; i<3;i++){
+            for(int j = 0; j<3;j++){
+                matrix_sum[i][j] = (int)(matrix1[i][j] * matrix2[i][j]);
+            }
+        }
+        matrixDisplay(matrix_sum);
+        
+    }
+
+    public static void Q5_division(){
+        
+        int [][] matrix1 = matrixGen(3,3);
+        matrixDisplay(matrix1);
+        int [][] matrix2 = matrixGen(3, 3);
+        matrixDisplay(matrix2);
+        double [][] matrix_sum = new double[3][3];
+        for(int i = 0; i<3;i++){
+            for(int j = 0; j<3;j++){
+                if(matrix2[i][j] == 0){
+                    matrix_sum[i][j] = Double.NaN;
+                }else{
+                    matrix_sum[i][j] = matrix1[i][j] / matrix2[i][j];
+                }
+            }
+        }
+        for (int i=0;i<matrix_sum.length;i++){
+            for(int j = 0; j<matrix_sum[i].length;j++){
+                System.out.print(matrix_sum[i][j]+" ");
+            }
+            System.out.println();
+            
+        }
+        System.out.println();
+    }
+    
 
 }
+
+
+
+
--- Term1/src/test.java
+++ Term1/src/test.java
@@ -1,22 +1,33 @@
-import java.util.Scanner;
 
+// Import the Arrays class from the java.util package.
+import java.util.Arrays;
+
+// Define a class named Q1.
 public class test {
+
+    // The main method where the program execution starts.
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Please enter the value of x: ");
-        double x=sc.nextDouble();
-        System.out.println("Please enter number of iterations: ");
-        int n=sc.nextInt();
-        double cosx=1;// initial value
-        for (int i=2;i<=n;i++){
-            double factorial=1;
-            for (int j=2*(i-1);j>=1;j--){
-                factorial=factorial*j;
-            }
-            double num1=Math.pow(-1,i-1);
-            double num2=Math.pow(x,2*(i-1));
-            cosx=cosx+(num1*num2/factorial);
+        // Declare and initialize an integer array 'my_array'.
+        int[] my_array = { 25, 14, 56, 15, 36, 56, 77, 18, 29, 49 };
+
+        // Define the position where the new element will be inserted.
+        int Index_position = 2;
+
+        // Define the value of the new element to be inserted.
+        int newValue = 5;
+        // Print the original array using Arrays.toString() method.
+        System.out.println("Original Array : " +
+                Arrays.toString(my_array));
+
+        // Loop to shift elements to make space for the new element.
+        for (int i = my_array.length - 1; i > Index_position; i--) {
+            my_array[i] = my_array[i - 1];
         }
-        System.out.println("Value of sin("+x+") is: "+ cosx);
+
+        // Insert the new element at the specified position.
+        my_array[Index_position] = newValue;
+
+        // Print the modified array with the new element.
+        System.out.println("New Array: " + Arrays.toString(my_array));
     }
 }
